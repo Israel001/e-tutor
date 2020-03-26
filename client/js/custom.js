fa8bd3a17346c2da3d649b8b895077461700e3c0
Son1812 <thesunday007@gmail.com>
2020-03-26 18:58:36 +0700

    Redesigned the chat

--- client/js/custom.js
+++ client/js/custom.js
@@ -13,9 +13,19 @@ $( ".bell" ).click(function() {
 	$( ".notify" ).slideToggle( "slow", function() {
 	});
 });
+
+function scrollb() {
+     var objDiv = document.getElementById("chat-messages");
+     objDiv.scrollTop = objDiv.scrollHeight;
+
+}
+$(document).ready(function(){
+    $('#chat-messages').animate({
+        scrollTop: $('#chat-messages')[0].scrollHeight}, 2000);
+});
+
 function openmenu() {
 	document.getElementById('side-menu').style.width="75%";
-	
 }
 function closemenu() {
 	document.getElementById('side-menu').style.width="0px";
@@ -28,7 +38,6 @@ function viewBylist() {
   document.getElementById("listmode").style.display="block";
   document.getElementById("gridmode").style.display="none";
 }
-
 $(document).ready(function(){
 	$('.slide-banner').slick({
 		slidesToShow: 1,
@@ -322,7 +331,7 @@ $(document).ready(function(){
 			$("#profile p").html(name);
 			$("#profile span").html(email);			
 			
-			$(".message").not(".right").find("img").attr("src", $(clone).attr("src"));									
+			$(".message").not(".right, #unfloat").find("img").attr("src", $(clone).attr("src"));								
 			$('#friendslist').fadeOut();
 			$('#chatview').fadeIn();
 		
@@ -932,3 +941,138 @@ ReactDOM.render(React.createElement(Calendar, null), document.getElementById("ap
   }).
   on("select selectstart", function (evt) {event.preventDefault();return false;});
 })(jQuery);
+
+
+/*-----------upload-------------*/
+var AttachmentArray = [];
+var arrCounter = 0;
+var input = $("#upload__input").val();
+(function($) {  
+  function createPdfPreview(fileContents, $displayElement) {
+    PDFJS.getDocument(fileContents).then(function(pdf) {
+      pdf.getPage(1).then(function(page) {
+        var $previewContainer = $displayElement.find('.preview__thumb');
+        var canvas = $previewContainer.find('canvas')[0];
+        canvas.height = $previewContainer.innerHeight();
+        canvas.width = $previewContainer.innerWidth();
+
+        var viewport = page.getViewport(1);
+        var scaleX = canvas.width / viewport.width;
+        var scaleY = canvas.height / viewport.height;
+        var scale = (scaleX < scaleY) ? scaleX : scaleY;
+        var scaledViewport = page.getViewport(scale);
+
+        var context = canvas.getContext('2d');
+        var renderContext = {
+          canvasContext: context,
+          viewport: scaledViewport
+        };
+        page.render(renderContext);
+      });
+    });
+  }
+  
+  
+  
+  
+  
+  function createPreview(file, fileContents, id) {
+    var $previewElement = '';
+    switch (file.type) {
+      case 'image/png':
+      case 'image/jpeg':
+      case 'image/gif':
+        $previewElement = $('<img src="' + fileContents + '" data-id="preview_' + id + '" />');
+        break;
+      case 'video/mp4':
+      case 'video/webm':
+      case 'video/ogg':
+        $previewElement = $('<video autoplay muted width="100%" height="100%"><source src="' + fileContents + '" type="' + file.type + '"></video>');
+        break;
+      case 'application/pdf':
+        $previewElement = $('<canvas id="" width="100%" height="100%"></canvas>');
+        break;
+      default:
+        break;
+    }
+    var $displayElement = $('<div class="preview" id="preview_'+ id +'">\
+                                <div class="preview__item">\
+                                <span class="closes">x</span>\
+                                <div class="preview__thumb"></div>\
+                                <span class="preview__name" title="' + file.name + '">' + file.name + '</span>\
+                                <span class="preview__type" title="' + file.type + '">' + file.type + '</span>\
+                                </div>\
+                            </div>');
+    $displayElement.find('.preview__thumb').append($previewElement);
+    $('.upload__files').append($displayElement);
+    
+    if (file.type === 'application/pdf') {
+      createPdfPreview(fileContents, $displayElement);
+    }
+  }
+  
+  
+  
+  
+  
+  function fileInputChangeHandler(e) {
+    var URL = window.URL || window.webkitURL;
+    var fileList = e.target.files;
+    if (fileList.length > 0) {
+      $('.upload__files').html('');
+      
+      for (var i = 0; i < fileList.length; i++) {
+        var file = fileList[i];
+        var fileUrl = URL.createObjectURL(file);
+        createPreview(file, fileUrl, i);
+        FillAttachmentArray(file)
+      }
+      console.log(AttachmentArray);
+    }
+  }
+  function FillAttachmentArray(file)
+        {
+            AttachmentArray[arrCounter] =
+            {
+                AttachmentType: 1,
+                ObjectType: 1,
+                FileName: file.name,
+                FileDescription: "Attachment",
+                NoteText: "",
+                MimeType: file.type,
+                FileSizeInBytes: file.size,
+            };
+            arrCounter = arrCounter + 1;
+        }
+  $(document).ready(function() {
+    $('input:file').on('change', fileInputChangeHandler);
+  });
+})(jQuery.noConflict());
+
+jQuery(function ($) {
+            $('div').on('click', '.preview__item .closes', function () {
+                var id = $(this).closest('.preview__item').find('img').data('id');
+
+                //to remove the deleted item from array
+                 var elementPos = AttachmentArray.map(function (x) { return x.FileName; }).indexOf(id);
+                 if (elementPos !== -1) {
+                     AttachmentArray.splice(elementPos, 1);
+                 }
+                
+                
+
+                // console.log(elementPos);
+
+                //to remove image tag
+                $(this).parent().find('img').not().remove();
+
+                //to remove div tag that contain the image
+                $(this).parent().find('div').not().remove();
+
+                //to remove div tag that contain caption name
+                $(this).parent().parent().find('div').not().remove();
+
+                $('#'+ id ).remove();
+              });
+        });
+  
