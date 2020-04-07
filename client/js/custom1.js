723bc059d4023f9b5006d1e3e8d732235406eead
Son1812 <thesunday007@gmail.com>
2020-04-07 15:16:51 +0700

    Added Admin Page

---
+++ client/js/custom1.js
@@ -0,0 +1,77 @@
+/*chart*/
+// Themes begin
+am4core.useTheme(am4themes_animated);
+// Themes end
+
+// Create chart instance
+var chart = am4core.create("chartdiv", am4charts.XYChart);
+
+// Add data
+chart.data = [{
+  "type": "Student",
+  "count": 15
+}, {
+  "type": "Tutor",
+  "count": 32
+}, {
+  "type": "Issue",
+  "count": 23
+}];
+
+// Create axes
+
+var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+categoryAxis.dataFields.category = "type";
+categoryAxis.renderer.grid.template.location = 0;
+categoryAxis.renderer.minGridDistance = 30;
+
+categoryAxis.renderer.labels.template.adapter.add("dy", function(dy, target) {
+  if (target.dataItem && target.dataItem.index & 2 == 2) {
+    return dy + 25;
+  }
+  return dy;
+});
+
+var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+
+// Create series
+var series = chart.series.push(new am4charts.ColumnSeries());
+series.dataFields.valueY = "count";
+series.dataFields.categoryX = "type";
+series.name = "Count";
+series.columns.template.tooltipText = "{categoryX}: [bold]{valueY}[/]";
+series.columns.template.fillOpacity = .8;
+
+var columnTemplate = series.columns.template;
+columnTemplate.strokeWidth = 2;
+columnTemplate.strokeOpacity = 1;
+
+
+/*-----------------------------------------------------------------*/
+function openmenu() {
+  document.getElementById('side-menu').style.width="75%";
+  document.getElementById('side-menu').style.marginLeft="0";
+}
+function closemenu() {
+  document.getElementById('side-menu').style.width="0px";
+  document.getElementById('side-menu').style.marginLeft="-255px";
+}
+
+/*--------------------------------------------------------------*/
+$("div[id^='myModal']").each(function(){
+  
+  var currentModal = $(this);
+  
+  //click next
+  currentModal.find('.btn-next').click(function(){
+    currentModal.modal('hide');
+    currentModal.closest("div[id^='myModal']").nextAll("div[id^='myModal']").first().modal('show'); 
+  });
+  
+  //click prev
+  currentModal.find('.btn-prev').click(function(){
+    currentModal.modal('hide');
+    currentModal.closest("div[id^='myModal']").prevAll("div[id^='myModal']").first().modal('show'); 
+  });
+
+});
