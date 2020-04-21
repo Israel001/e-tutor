e8d27e28e6c45370865e886a23b3a722100573ca
PBNL <israelobanijesu2@gmail.com>
2020-04-21 13:37:31 +0700

    Set Google Auth Redirect URL Dynamicity

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -10,7 +10,7 @@ const OAuth2Data = require('../google_key');
 
 const CLIENT_ID = OAuth2Data.web.client_id;
 const CLIENT_SECRET = OAuth2Data.web.client_secret;
-const REDIRECT_URL = OAuth2Data.web.redirect_uris[0];
+const REDIRECT_URL = process.env.PORT ? OAuth2Data.web.redirect_uris[1] : OAuth2Data.web.redirect_uris[0];
 
 const OAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
 
